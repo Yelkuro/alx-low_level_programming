ff410acc07cd80331de0f193982289d0dacfc2b2
Yelkuro <myelkuro@gmail.com>
2023-06-15 18:16:45 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - A C program that prints the size of compiled programs
+ * main - Entry point of the program
  *
- * Return: Always 0 (success)
+ * Description: Prints the sizes of various types on the computer it is compiled and run on.
  *
+ * Return: Always 0 (indicating success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+    printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of short: %zu byte(s)\n", sizeof(short));
+    printf("Size of long: %zu byte(s)\n", sizeof(long));
+    printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
+    printf("Size of double: %zu byte(s)\n", sizeof(double));
+    printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 
-	printf("size of char: %zu byte(s)\n" (unsigned long)sizeof(a));
-	printf("size of int: %zu byte(s)\n" (unsigned long)sizeof(b));
-	printf("size of long int: %zu byte(s)\n" (unsigned long)sizeof(c));
-	printf("size of long long int: %zu byte(s)\n" (unsigned long)sizeof(d));
-	printf("size of float: %zu byte(s)\n" (unsigned long)sizeof(f));
-
-	return (0);
+    return (0);
 }
+

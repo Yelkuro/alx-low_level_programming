efb284ecd550a91ddcad78b5fa20e995287d77e8
Yelkuro <myelkuro@gmail.com>
2023-06-15 19:13:52 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main entry here
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stduot);
+	return (1);
+}
